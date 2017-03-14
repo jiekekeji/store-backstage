d5fd999ad5ede0b4dd1816f1e622c3062b349599
jiekekeji <jieke123>
2017-03-14 17:45:47 +0800

    修改搜索框IE不兼容的问题

--- src/components/PdFunSet.vue
+++ src/components/PdFunSet.vue
@@ -1,18 +1,22 @@
 <template>
-    <div>
-      asdfasfdafd
-    </div>
+  <div>
+    <button @click="open({name:'jack'})">试试</button>
+  </div>
 </template>
 <style>
 </style>
 <script>
-    export default{
-        data(){
-            return{
-                msg:'hello vue'
-            }
-        },
-        components:{
-        }
+  export default{
+    data(){
+      return {
+        msg: 'hello vue'
+      }
+    },
+    components: {},
+    methods: {
+      open(obj){
+        console.log(obj);
+      }
     }
+  }
 </script>
