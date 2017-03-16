016bd04f7c719d4050acbe75ecd6d93ea7f8cb90
jiekekeji <jieke123>
2017-03-16 10:55:29 +0800

    商品类别查找

--- src/components/PdList.vue
+++ src/components/PdList.vue
@@ -1,18 +1,26 @@
 <template>
-    <div>
-      asdfasfdafd
-    </div>
+  <div>
+    <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1">处理中心</el-menu-item>
+      <el-menu-item index="2">处理中心2</el-menu-item>
+      <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+    </el-menu>
+  </div>
 </template>
 <style>
 </style>
 <script>
-    export default{
-        data(){
-            return{
-                msg:'hello vue'
-            }
-        },
-        components:{
-        }
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(index) {
+        this.$router.push(index);
+      }
     }
+  }
 </script>
